63bbc471facfe1136f50df4f6eff0089ccdbbcd7
aochan <nakinagaraodoru1013@gmail.com>
2024-10-23 03:07:51 +0000

    close #11

--- app/resolver.go
+++ app/resolver.go
@@ -13,8 +13,10 @@ func NewResolver(board Board) *Resolver {
 }
 
 func (r *Resolver) resolve() {
+	emptySpaces := r.board.SearchNotYetEntered()
+
 	for _, input := range []uint8{1, 2, 3, 4, 5, 6, 7, 8, 9} {
-		r.board = r.board.FillIn(0, 0, input)
+		r.board = r.board.FillIn(emptySpaces[0][0], emptySpaces[0][1], input)
 
 		if NewChecker(r.board).IsComplete() {
 			r.IsComplete = true
--- app/resolver_test.go
+++ app/resolver_test.go
@@ -6,11 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestResolve(t *testing.T) {
-	sut := NewResolver(
-		*NewBoard(
-			[9][9]uint8{
-				{0, 1, 9, 5, 4, 3, 6, 7, 8},
+func TestResolveLastNumberSpace(t *testing.T) {
+	cases := []struct {
+		name  string
+		board [9][9]uint8
+	}{
+		{
+			name: "empty 0, 0",
+			board: [9][9]uint8{
+				{NOT_YET_INPUT, 1, 9, 5, 4, 3, 6, 7, 8},
 				{5, 4, 3, 8, 7, 6, 9, 1, 2},
 				{8, 7, 6, 2, 1, 9, 3, 4, 5},
 				{4, 3, 2, 7, 6, 5, 8, 9, 1},
@@ -20,10 +24,44 @@ func TestResolve(t *testing.T) {
 				{6, 5, 4, 9, 8, 7, 1, 2, 3},
 				{9, 8, 7, 3, 2, 1, 4, 5, 6},
 			},
-		),
-	)
+		},
+		{
+			name: "empty 0, 7",
+			board: [9][9]uint8{
+				{2, 1, 9, 5, 4, 3, 6, NOT_YET_INPUT, 8},
+				{5, 4, 3, 8, 7, 6, 9, 1, 2},
+				{8, 7, 6, 2, 1, 9, 3, 4, 5},
+				{4, 3, 2, 7, 6, 5, 8, 9, 1},
+				{7, 6, 5, 1, 9, 8, 2, 3, 4},
+				{1, 9, 8, 4, 3, 2, 5, 6, 7},
+				{3, 2, 1, 6, 5, 4, 7, 8, 9},
+				{6, 5, 4, 9, 8, 7, 1, 2, 3},
+				{9, 8, 7, 3, 2, 1, 4, 5, 6},
+			},
+		},
+		{
+			name: "empty 3, 4",
+			board: [9][9]uint8{
+				{2, 1, 9, 5, 4, 3, 6, 7, 8},
+				{5, 4, 3, 8, 7, 6, 9, 1, 2},
+				{8, 7, 6, 2, 1, 9, 3, 4, 5},
+				{4, 3, 2, 7, NOT_YET_INPUT, 5, 8, 9, 1},
+				{7, 6, 5, 1, 9, 8, 2, 3, 4},
+				{1, 9, 8, 4, 3, 2, 5, 6, 7},
+				{3, 2, 1, 6, 5, 4, 7, 8, 9},
+				{6, 5, 4, 9, 8, 7, 1, 2, 3},
+				{9, 8, 7, 3, 2, 1, 4, 5, 6},
+			},
+		},
+	}
 
-	sut.resolve()
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			sut := NewResolver(*NewBoard(tt.board))
 
-	assert.Equal(t, true, sut.IsComplete)
+			sut.resolve()
+
+			assert.Equal(t, true, sut.IsComplete)
+		})
+	}
 }
