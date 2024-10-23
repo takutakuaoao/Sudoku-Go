342cfcf098b762142a98a7faeefa6cde766b23fe
aochan <nakinagaraodoru1013@gmail.com>
2024-10-23 10:41:12 +0000

    close #19

--- app/history.go
+++ app/history.go
@@ -2,14 +2,26 @@ package app
 
 func NewHistory(positions [][2]uint8) *History {
 	return &History{
-		positions: positions,
+		positions:       positions,
+		currentPosition: positions[0],
+		currentNumber:   1,
 	}
 }
 
 type History struct {
-	positions [][2]uint8
+	positions       [][2]uint8
+	currentPosition [2]uint8
+	currentNumber   uint8
 }
 
-func (h *History) GetPosition() (uint8, uint8) {
-	return h.positions[0][0], h.positions[0][1]
+func (h History) NG() History {
+	return History{
+		positions:       h.positions,
+		currentPosition: h.currentPosition,
+		currentNumber:   h.currentNumber + 1,
+	}
+}
+
+func (h History) GetInput() ([2]uint8, uint8) {
+	return h.currentPosition, h.currentNumber
 }
--- app/history_test.go
+++ app/history_test.go
@@ -9,8 +9,17 @@ import (
 func TestStart(t *testing.T) {
 	sut := NewHistory([][2]uint8{{1, 0}})
 
-	y, x := sut.GetPosition()
+	position, number := sut.GetInput()
 
-	assert.Equal(t, uint8(1), y)
-	assert.Equal(t, uint8(0), x)
+	assert.Equal(t, [2]uint8{1, 0}, position)
+	assert.Equal(t, uint8(1), number)
+}
+
+func TestNG(t *testing.T) {
+	sut := NewHistory([][2]uint8{{2, 0}})
+
+	position, number := sut.NG().GetInput()
+
+	assert.Equal(t, position, [2]uint8{2, 0})
+	assert.Equal(t, number, uint8(2))
 }
