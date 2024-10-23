1a0dedcb9fca2f5a894de892986525e066fe0451
aochan <nakinagaraodoru1013@gmail.com>
2024-10-23 13:06:09 +0000

    close #18

--- app/history.go
+++ app/history.go
@@ -1,27 +1,104 @@
 package app
 
 func NewHistory(positions [][2]uint8) *History {
+	_positions := []position{}
+
+	for _, _position := range positions {
+		_positions = append(_positions, *newPosition(_position))
+	}
+
 	return &History{
-		positions:       positions,
-		currentPosition: positions[0],
-		currentNumber:   1,
+		currentIndex: 0,
+		positions:    _positions,
 	}
 }
 
 type History struct {
-	positions       [][2]uint8
-	currentPosition [2]uint8
-	currentNumber   uint8
+	currentIndex uint8
+	positions    []position
+}
+
+func (h History) OK() History {
+	return h.nextPosition()
 }
 
 func (h History) NG() History {
-	return History{
-		positions:       h.positions,
-		currentPosition: h.currentPosition,
-		currentNumber:   h.currentNumber + 1,
+	if h.getCurrent().hasLast() {
+		return h.backPosition()
 	}
+
+	return h.nextNumber()
 }
 
 func (h History) GetInput() ([2]uint8, uint8) {
-	return h.currentPosition, h.currentNumber
+	return h.getCurrent().toPrimitive()
+}
+
+func (h History) nextNumber() History {
+	return h.updatePosition(h.currentIndex, h.getCurrent().nextNumber())
+}
+
+func (h History) nextPosition() History {
+	return h.updateIndex(h.currentIndex + 1)
+}
+
+func (h History) backPosition() History {
+	return h.
+		updatePosition(h.currentIndex-1, h.getPrevPosition().nextNumber()).
+		updateIndex(h.currentIndex - 1)
+}
+
+func (h History) getCurrent() position {
+	return h.positions[h.currentIndex]
+}
+
+func (h History) getPrevPosition() position {
+	return h.positions[h.currentIndex-1]
+}
+
+func (h History) updatePosition(index uint8, newPos position) History {
+	tmpPositions := h.positions
+	tmpPositions[index] = newPos
+
+	return History{
+		currentIndex: h.currentIndex,
+		positions:    tmpPositions,
+	}
+}
+
+func (h History) updateIndex(index uint8) History {
+	return History{
+		positions:    h.positions,
+		currentIndex: index,
+	}
+}
+
+type position struct {
+	y          uint8
+	x          uint8
+	lastNumber uint8
+}
+
+func newPosition(pos [2]uint8) *position {
+	return &position{
+		y:          pos[0],
+		x:          pos[1],
+		lastNumber: 1,
+	}
+}
+
+func (p position) nextNumber() position {
+	return position{
+		y:          p.y,
+		x:          p.x,
+		lastNumber: p.lastNumber + 1,
+	}
+}
+
+func (p position) hasLast() bool {
+	return p.lastNumber == 9
+}
+
+func (p position) toPrimitive() ([2]uint8, uint8) {
+	return [2]uint8{p.y, p.x}, p.lastNumber
 }
--- app/history_test.go
+++ app/history_test.go
@@ -15,10 +15,19 @@ func TestStart(t *testing.T) {
 	assert.Equal(t, uint8(1), number)
 }
 
-func TestNG(t *testing.T) {
+func Test_return_next_number_if_number_other_then_the_last_is_NG(t *testing.T) {
 	sut := NewHistory([][2]uint8{{2, 0}})
 
 	position, number := sut.NG().GetInput()
+
+	assert.Equal(t, position, [2]uint8{2, 0})
+	assert.Equal(t, number, uint8(2))
+}
+
+func Test_if_the_last_number_nine_is_NG_the_history_return_prev_position_and_next_number_of_prev_success_number(t *testing.T) {
+	sut := NewHistory([][2]uint8{{2, 0}, {3, 1}})
+
+	position, number := sut.OK().NG().NG().NG().NG().NG().NG().NG().NG().NG().GetInput()
 
 	assert.Equal(t, position, [2]uint8{2, 0})
 	assert.Equal(t, number, uint8(2))
