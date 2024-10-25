906c6420ce717ac5514d8a19ec93ba029cf23a62
aochan <nakinagaraodoru1013@gmail.com>
2024-10-25 11:20:08 +0000

    🚀 Add the test for solving sudoku that is most difficult in the world.

--- app/resolver_test.go
+++ app/resolver_test.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -76,30 +75,24 @@ func Test_resolve_beginner(t *testing.T) {
 }
 
 func Test_resolve_easy(t *testing.T) {
-	t.Skip("It takes too much time")
 	sut := NewResolver(*NewBoard([9][9]uint8{
-		{3, 2, 6, 0, 0, 1, 0, 0, 0},
-		{7, 9, 1, 0, 0, 0, 0, 0, 5},
-		{5, 4, 8, 6, 9, 2, 3, 6, 1},
-		{6, 7, 3, 2, 1, 4, 8, 0, 0},
+		{3, 2, 0, 0, 0, 1, 0, 0, 0},
+		{7, 0, 0, 0, 0, 0, 0, 0, 5},
+		{0, 4, 8, 6, 9, 2, 0, 0, 1},
+		{0, 7, 0, 2, 0, 0, 8, 0, 0},
 		{1, 8, 2, 9, 0, 0, 0, 3, 4},
-		{4, 5, 9, 3, 8, 6, 0, 1, 0},
+		{0, 0, 9, 0, 0, 6, 0, 1, 0},
 		{0, 0, 5, 0, 0, 0, 1, 8, 6},
 		{0, 0, 4, 0, 0, 0, 0, 0, 0},
-		{9, 0, 7, 8, 6, 0, 4, 0, 0},
+		{9, 0, 0, 8, 6, 0, 4, 0, 0},
 	}))
 
 	result := sut.Resolve()
-
-	for _, row := range result.board.rows {
-		fmt.Println(row)
-	}
 
 	assert.True(t, result.IsComplete())
 }
 
 func Test_have_them_solve_advanced_problem(t *testing.T) {
-	t.Skip("It takes too much time")
 	sut := NewResolver(*NewBoard([9][9]uint8{
 		{0, 0, 6, 0, 0, 0, 2, 0, 0},
 		{0, 2, 0, 0, 0, 0, 9, 0, 8},
@@ -110,6 +103,24 @@ func Test_have_them_solve_advanced_problem(t *testing.T) {
 		{0, 0, 0, 9, 0, 0, 0, 0, 6},
 		{0, 4, 1, 0, 3, 0, 0, 0, 0},
 		{8, 0, 0, 6, 0, 0, 0, 0, 0},
+	}))
+
+	result := sut.Resolve()
+
+	assert.True(t, result.IsComplete())
+}
+
+func Test_have_them_solve_very_hard_problem(t *testing.T) {
+	sut := NewResolver(*NewBoard([9][9]uint8{
+		{8, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 3, 6, 0, 0, 0, 0, 0},
+		{0, 7, 0, 0, 9, 0, 2, 0, 0},
+		{0, 5, 0, 0, 0, 7, 0, 0, 0},
+		{0, 0, 0, 0, 4, 5, 7, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0, 3, 0},
+		{0, 0, 1, 0, 0, 0, 0, 6, 8},
+		{0, 0, 8, 5, 0, 0, 0, 1, 0},
+		{0, 9, 0, 0, 0, 0, 4, 0, 0},
 	}))
 
 	result := sut.Resolve()
